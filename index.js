123897310799b97b7f7a0d120c6f7c0d85c99a5d
Shawn M Carlson <157416132+OxMagnus@users.noreply.github.com>
2024-04-08 22:02:12 -0400

    final touches

--- index.js
+++ index.js
@@ -14,7 +14,42 @@ const questions = [
     name: 'description',
     message: 'Please provide a brief description of your project:'
   },
-  // Add more questions as needed
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'How do you install your app?'
+  },
+  {
+    type: 'input',
+    name: 'instructions',
+    message: 'Instructions to be followed:'
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Any credits?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use your app?'
+  },
+  {
+    type: 'list',
+    name:'license',
+    message: 'What license was used?',
+    choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0','Apache License 2.0','MIT License', 'N/A']
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'GitHub Username:'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'E-mail:'
+  }
 ];
 
 // Create a function to write README file
@@ -43,15 +78,62 @@ function init() {
 
 // Function to generate README content based on user input
 function generateReadme(data) {
+  const licenseBadgeUrl = licenseBadge(data.license);
   return `
-# ${data.title}
+  ${licenseBadgeUrl}
+# ${data.title} 
+
 
 ## Description
+
 ${data.description}
 
+## Table of Contents
+* [Installation](#installation)
+* [Instructions](#instructions)
+* [Contribution](#contribution)
+* [Usage](#usage)
+* [Contact](#contact)
+
+## Installation
+${data.installation}
+
+## Instructions
+${data.instructions}
+
+## Contribution
+${data.contribution}
+
+## Usage
+${data.usage}
+
+
+## Contact
+* GitHub: [${data.username}](https://github.com/${data.username})
+* E-mail: ${data.email}
 `;
-  // Add more sections based on user input if needed
+}
+
+// Function to generate license badge URL based on license type
+function licenseBadge(licenseType) {
+  switch (licenseType) {
+    case 'GNU AGPLv3':
+      return '![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)';
+    case 'GNU GPLv3':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)';
+    case 'GNU LGPLv3':
+      return '![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)';
+    case 'Mozilla Public License 2.0':
+      return '![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)';
+    case 'Apache License 2.0':
+      return '![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+    case 'MIT License':
+      return '![License: MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    default:
+      return 'License information not available';
+  }
 }
 
 // Function call to initialize app
 init();
+
